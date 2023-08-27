72e2639187d31605362acb6cf98a848c59ed5216
CAyanfe <abiodunfaith15@gmail.com>
2023-08-27 13:05:05 +0100

    ReactNew2

---
+++ src/components/Cards.js
@@ -0,0 +1,21 @@
+import { Button, Card, CardGroup } from "react-bootstrap";
+
+export const CardDesign = (props) => {
+  const { img, Product, Price, desc, link } = props;
+  return (
+    <CardGroup>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{Product}</Card.Title>
+          <Card.Text>{desc}</Card.Text>
+          <Card.Text>{Price}</Card.Text>
+          <Button variant="primary">{link}</Button>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+  );
+};
